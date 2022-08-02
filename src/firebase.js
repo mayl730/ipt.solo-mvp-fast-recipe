1e730f7d1187d107db76162943c18ddb86b46adf
mayl730 <maylaixyz@gmail.com>
2022-08-02 21:45:43 +0900

    remove: firebase config in the source code

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,6 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "AIzaSyAyj3Q3HdIEToT_IY8gxIxzSDWt_cVSZOs",
-  // authDomain: "fast-recipe-7aa79.firebaseapp.com",
-  // projectId: "fast-recipe-7aa79",
-  // storageBucket: "fast-recipe-7aa79.appspot.com",
-  // messagingSenderId: "54427158958",
-  // appId: "1:54427158958:web:77ab9bc6a6c6e76dfe603f",
-  // measurementId: "G-1E84GZ7KR2"
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
