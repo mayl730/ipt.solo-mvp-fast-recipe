606561b9dcce462e4cedab263506c6556500ae04
mayl730 <maylaixyz@gmail.com>
2022-09-11 20:38:21 +0900

    add: install material icon

--- src/components/ButtonIcon.js
+++ src/components/ButtonIcon.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 export default function ButtonIcon({
   type = 'submit',
@@ -14,7 +15,7 @@ export default function ButtonIcon({
       }
       disabled={processing}
     >
-     <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+  
       <span>{children}</span>
     </button>
   );
