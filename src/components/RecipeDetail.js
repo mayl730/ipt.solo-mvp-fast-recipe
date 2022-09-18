5320e1dd308196bdb06f83887aeaeec45d5a5514
mayl730 <maylaixyz@gmail.com>
2022-09-18 22:24:53 +0900

    feat: implement style for recipe detail page

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -3,21 +3,17 @@ import React from "react"
 import { Container, Button, Col, Row } from 'react-bootstrap';
 import { ButtonConfirm, ButtonIcon } from './ui/Buttons';
 import { removeRecipe } from '../utils/index';
-import { MdModeEdit, MdDeleteForever } from "react-icons/md";
+import { MdModeEdit, MdDeleteForever, MdAccessTime } from "react-icons/md";
 
 export default function RecipeDetail(props) {
   const { selectedRecipe, selectedRecipeIngredients } = props
   return (
-    <div className="container mx-auto"
+    <div className="container mx-auto mb-10"
          key = {selectedRecipe.id}>
-
-       
-     
-        <br></br>
  
-        <div className="flex flex-col mt-5 desktop:flex-row desktop:space-x-5">
-          <h3 className="font-montserrat text-4xl text-black-800 my-3">{selectedRecipe.title}</h3>  
-          <div className="my-2">
+        <div className="mx-5 flex flex-col mt-0 desktop:flex-row desktop:space-x-5">
+          <h3 className="font-montserrat text-4xl text-black-800 desktop:my-7">{selectedRecipe.title}</h3>  
+          <div className="desktop:my-6">
             <Link to="/edit">
                 <ButtonIcon
                       className="mr-2"
@@ -38,27 +34,52 @@ export default function RecipeDetail(props) {
             </Link> 
           </div>
         </div>
+        
+        <div className="mx-5 desktop:mb-10 text-base font-cabin text-black-800">
+          <MdAccessTime className="inline mr-1.5"></MdAccessTime>
+          <span className="inline align-middle">38 minutes</span>
+          <p className="inline align-middle mx-5">|</p>
+          <p className="inline align-middle">{selectedRecipe.calories} kcal</p>
+        </div>
 
-        <img src={selectedRecipe.image} alt="food"></img>
+        <div className="mx-5 mb-10">
+          <img src={selectedRecipe.image} alt="food" className="rounded-2xl"></img>
+          <p className="font-cabin text-lg text-black-800 leading-8 mt-5">{selectedRecipe.description}</p>
+          <h4 className="font-montserrat text-2xl text-black-800 my-5">Ingredients</h4>
+          
+          <ul class="tracking-wide font-cabin text-lg text-black-800 max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+          { selectedRecipeIngredients.map((item, index) => {
+            const content = (
+                <>
+                <span>{item.name}</span>
+                <span> : </span>
+                <span>{item.amount}</span>
+                </>
+            )
+            if (index === 0) {
+              return (
+                <li class="pb-3 sm:pb-4">{content}</li>
+                )
+            }
 
-       <h4>Description</h4>
-       <p>{selectedRecipe.description}</p>
+            if (index === selectedRecipeIngredients.length-1) {
+              return (
+                <li class="pt-3 pb-0 sm:pt-4">{content}</li>
+                )
+            }
+            
+            return (
+              <li class="py-3 sm:py-4">{content}</li>
+              );
+            }) }
+          </ul>
+        </div>
 
-       <h4>Ingredients</h4>
-       { selectedRecipeIngredients.map((item) => {
-        return (
-          <div>
-            <span>{item.name}</span>
-            <span>: </span>
-            <span>{item.amount}</span>
-          </div>
-        )
-       }) }
-       <h4>Calories</h4>
-       <p>{selectedRecipe.calories}kcal</p>
-
-    <h4>Instructions</h4>
-    <p class="recipe__instructions">{selectedRecipe.instruction}</p>
+        <div className="mx-5 mb-10">
+           <h4 className="font-montserrat text-2xl text-black-800 my-5">Instructions</h4>
+           <p className="font-cabin text-lg text-black-800 leading-8 mt-5 whitespace-pre-wrap">{selectedRecipe.instruction}</p>
+        </div>
+        
 
     </div>
   );
